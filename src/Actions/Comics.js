5b111885b73e94c76b0702c5fd2a623b2728f2d3
Anthony Rojas <anthony.rojas095@gmail.com>
2019-03-25 22:57:05 -0700

    comic actions and reducers progress, still need to work on UI component

--- src/Actions/Comics.js
+++ src/Actions/Comics.js
@@ -4,7 +4,19 @@ import {
     GET_COMICS_ATTEMPT,
     GET_COMICS_FAILURE,
     GET_COMICS_SUCCESS,
-    ADD_COMIC_ATTEMPT
+    ADD_COMIC_ATTEMPT,
+    ADD_COMIC_SUCCESS,
+    ADD_COMIC_FAILURE,
+    GET_COMIC_ATTEMPT,
+    GET_COMIC_SUCCESS,
+    GET_COMIC_FAILURE,
+    COMIC_TITLE_CHANGED,
+    COMIC_DESCRIPTION_CHANGED,
+    COMIC_FIVE_DIGIT_ID_CHANGED,
+    COMIC_UPC_CHANGED,
+    COMIC_PUBLISHER_CHANGED,
+    COMIC_CREATORS_CHANGED,
+    COMIC_CONDITION_CHANGED
 } from './types';
 import {HOST, EMPTY_ARR, EMPTY_STR} from '../constants';
 
@@ -47,7 +59,96 @@ export const addComicAttempt = (data)=>{
             }
         };
         axios.post(`${HOST}/api/comics`, data.comicData, config)
-        .then(res=>{})
-        .catch(err=>{});
+        .then(res=>{
+            dispatch({
+                type: ADD_COMIC_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err=>{
+            let failData = (
+                err.response !== undefined ? 
+                err.reponse.data.statusMessage :
+                'No internet connection.'
+            )
+            dispatch({
+                type: ADD_COMIC_FAILURE,
+                payload: failData
+            });
+        });
     }
+}
+export const getComicAttempt = (data)=>{
+    return(dispatch)=>{
+        dispatch({
+            type: GET_COMIC_ATTEMPT,
+            payload: data.statusMessage
+        });
+        let config = {
+            headers: {
+                Authorization: localStorage.getItem('token')
+            }
+        }
+        axios.get(`${HOST}/api/comics/${data.comicId}`, config)
+        .then(res => {
+            dispatch({
+                type: GET_COMIC_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            let failData = (
+                err.response !== undefined ?
+                err.response.data.statusMessage : 
+                'No internet connection'
+            );
+            dispatch({
+                type: GET_COMIC_FAILURE,
+                payload: failData
+            });
+        });
+    }
+}
+export const getComicConditions = (data)=>{}
+export const comicTitleChanged = (data)=>{
+    return({
+        type: COMIC_TITLE_CHANGED,
+        payload: data
+    });
+}
+export const comicDescriptionChanged = (data)=>{
+    return({
+        type: COMIC_DESCRIPTION_CHANGED,
+        payload: data
+    });
+}
+export const comicFiveDigitIdChanged = (data)=>{
+    return({
+        type: COMIC_FIVE_DIGIT_ID_CHANGED,
+        payload: data
+    });
+}
+export const comicUPCChanged = (data)=>{
+    return({
+        type: COMIC_UPC_CHANGED,
+        payload: data
+    });
+}
+export const comicConditionChanged = (data)=>{
+    return({
+        type: COMIC_CONDITION_CHANGED,
+        payload: data
+    });
+}
+export const comicPublisherChanged = (data)=>{
+    return({
+        type: COMIC_PUBLISHER_CHANGED,
+        payload: data
+    });
+}
+export const comicCreatorsChanged = (data)=>{
+    return({
+        type: COMIC_CREATORS_CHANGED,
+        payload: data
+    });
 }
--- src/Reducers/Comics.js
+++ src/Reducers/Comics.js
@@ -1,14 +1,63 @@
 import {
     GET_COMICS_ATTEMPT,
     GET_COMICS_FAILURE,
-    GET_COMICS_SUCCESS
+    GET_COMICS_SUCCESS,
+    GET_COMIC_ATTEMPT,
+    COMIC_TITLE_CHANGED,
+    COMIC_DESCRIPTION_CHANGED,
+    COMIC_UPC_CHANGED,
+    COMIC_CONDITION_CHANGED,
+    COMIC_FIVE_DIGIT_ID_CHANGED,
+    COMIC_PUBLISHER_CHANGED,
+    COMIC_CREATORS_CHANGED,
+    COMIC_IMAGE_CHANGED_ATTEMPT,
+    COMIC_IMAGE_CHANGED_FAILURE,
+    COMIC_IMAGE_CHANGED_SUCCESS,
+    GET_COMIC_CONDITIONS_FAILURE,
+    GET_COMIC_CONDITIONS_SUCCESS,
+    GET_COMIC_CONDITIONS_ATTEMPT
 } from '../Actions/types';
-import {EMPTY_ARR, EMPTY_STR} from '../constants';
+import {EMPTY_ARR, EMPTY_STR, DEFAULT_NUM} from '../constants';
 const initialState = {
     comicsLoading: false,
+    comicLoading: false,
     comicsList: EMPTY_ARR,
+    comicsErr: EMPTY_STR,
+    comicErr: EMPTY_STR,
+    uploadingImage: false,
     statusMessage: action.payload,
-    error: EMPTY_STR
+    comicConditionsLoading: false,
+    comicConditions: EMPTY_ARR,
+    comic: {
+        title: EMPTY_STR,
+        description: EMPTY_STR,
+        UPC: DEFAULT_NUM,
+        //the five digit issue identifier
+        //[0-3]: issue number
+        //[4]: variant
+        //[5]: printing,
+        imageStr: EMPTY_STR,
+        fiveDigitId: DEFAULT_NUM,
+        condition: EMPTY_STR,
+        user: EMPTY_STR,
+        publisher:EMPTY_STR,
+        creators: EMPTY_ARR
+    },
+    errors: {
+        title: EMPTY_STR,
+        description: EMPTY_STR,
+        UPC: EMPTY_STR,
+        //the five digit issue identifier
+        //[0-3]: issue number
+        //[4]: variant
+        //[5]: printing,
+        imageStr: EMPTY_STR,
+        fiveDigitId: EMPTY_STR,
+        condition: EMPTY_STR,
+        user: EMPTY_STR,
+        publisher:EMPTY_STR,
+        creators: EMPTY_STR
+    }
 }
 export default (state = initialState, action)=>{
     switch(action.type){
@@ -23,7 +72,8 @@ export default (state = initialState, action)=>{
                 ...state,
                 comicsLoading: false,
                 comicsList: EMPTY_ARR,
-                statusMessage: action.payload
+                statusMessage: EMPTY_STR,
+                comicsErr: action.payload
             }
         case GET_COMICS_SUCCESS: 
             return{
@@ -31,6 +81,130 @@ export default (state = initialState, action)=>{
                 comicsList: action.payload.comics,
                 statusMessage: action.payload.statusMessage,
                 comicsLoading: false
+            }
+        case GET_COMIC_ATTEMPT:
+            return{
+                ...state,
+                comicLoading: true,
+                statusMessage: action.payload
+            }
+        case GET_COMIC_SUCCESS:
+            return{
+                ...state,
+                comicLoading: false,
+                comic: {
+                    title: action.payload.comic.title,
+                    description: action.payload.comic.description,
+                    UPC: action.payload.comic.UPC,
+                    imageStr: action.payload.comic.imageStr,
+                    fiveDigitId: action.payload.comic.fiveDigitId,
+                    condition: action.payload.comic.condition,
+                    user: action.payload.comic.user,
+                    publisher: action.payload.comic.publisher,
+                    creators: action.payload.comic.creators
+                },
+                statusMessage: action.payload.statusMessage
+            }
+        case GET_COMIC_FAILURE: 
+            return{
+                ...state,
+                comicLoading: false,
+                comicErr: action.payload,
+                statusMessage: EMPTY_STR
+            }
+        case COMIC_TITLE_CHANGED: 
+            return{
+                ...state,
+                comic: {
+                    title: action.payload
+                }
+            }
+        case COMIC_DESCRIPTION_CHANGED:
+            return{
+                ...state,
+                comic: {
+                    description: action.payload
+                }
+            }
+        case COMIC_UPC_CHANGED:
+            return{
+                ...state,
+                comic: {
+                    UPC: action.payload
+                }
+            }
+        case COMIC_CONDITION_CHANGED:
+            return{
+                ...state,
+                comic:{
+                    condition: action.payload
+                }
+            }
+        case COMIC_FIVE_DIGIT_ID_CHANGED:
+            return{
+                ...state,
+                comic:{
+                    fiveDigitId: action.payload
+                }
+            }
+        case COMIC_PUBLISHER_CHANGED: {
+            return{
+                ...state,
+                comic: {
+                    publisher: action.payload
+                }
+            }
+        }
+        case COMIC_CREATORS_CHANGED:
+            return{
+                ...state,
+                comic: {
+                    creators: action.payload
+                }
+            }
+        case COMIC_IMAGE_CHANGED_ATTEMPT:
+            return{
+                ...state,
+                uploadingImage: true,
+                statusMessage: action.payload,
+                errors: {
+                    imageStr: EMPTY_STR
+                }
+            }
+        case COMIC_IMAGE_CHANGED_FAILURE:
+            return{
+                ...state,
+                uploadingImage: false,
+                errors: {
+                    imageStr: action.payload
+                }
+            }
+        case COMIC_IMAGE_CHANGED_SUCCESS:
+            return{
+                ...state,
+                uploadingImage: false,
+                comic: {
+                    imageStr: action.payload
+                },
+                errors: {
+                    imageStr: EMPTY_STR
+                }
+            }
+        case GET_COMIC_CONDITIONS_ATTEMPT:
+            return{
+                ...state,
+                comicConditionsLoading: true
+            }
+        case GET_COMIC_CONDITIONS_FAILURE:
+            return{
+                ...state,
+                comicConditionsLoading: false
+            }
+        case GET_COMIC_CONDITIONS_SUCCESS:
+            return{
+                ...state,
+                comicConditionsLoading: false,
+                comicConditions: action.payload
             }
         default: state
     }
